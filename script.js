0c40a80fb562528142d2b2e53ff9d5db8da3b45b
Unsi236 <unsiyahmuhtaromah61820@gmail.com>
2024-03-30 08:03:18 +0700

    Tugas-JS-PWEBPR-C-3002

---
+++ script.js
@@ -0,0 +1,26 @@
+const boxpertanyaan = document.getElementById("boxpertanyaan")
+const kalimatpertanyaan = document.getElementById("kalimatpertanyaan");
+const pertanyaan = document.getElementById("pertanyaan");
+const luaran = document.getElementById("luaran")
+const gambardetektif = document.getElementById("gambardetektif");
+const buttonpertanyaan = document.getElementById("buttonpertanyaan");
+const benar = document.getElementById("benar");
+const salah = document.getElementById("salah");
+
+benar.addEventListener('click', ()=> {
+  kalimatpertanyaan.innerHTML = "Anda Berbohong!";
+  pertanyaan.innerHTML = "Anda adalah seorang Teknisi";
+  luaran.style.backgroundColor = "#E12E2E";
+  gambardetektif.src = "Assets/teknisi.png";
+  buttonpertanyaan.style.display = 'none';
+  boxpertanyaan.classList.add("animasigetar");
+});
+
+salah.addEventListener('click', ()=> {
+  kalimatpertanyaan.innerHTML = "Anda Benar!";
+  pertanyaan.innerHTML = "Anda adalah seorang Mahasiswa";
+  luaran.style.backgroundColor = "#6FD240"
+  gambardetektif.src = "Assets/mahasiswa.png";
+  buttonpertanyaan.style.display = 'none';
+  boxpertanyaan.classList.add("animasijj");
+});
